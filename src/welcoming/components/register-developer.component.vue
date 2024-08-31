dae78e4cde7272d6eda029a3216c99f4d14ce0e3
Daniel Del Castillo <u202211212@upc.edu.pe>
2024-08-31 12:01:27 -0500

    feat(register-developer): added developer component.

---
+++ src/welcoming/components/register-developer.component.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  name: "register-developer.component",
+  data(){
+    return{
+      firstName: "",
+      lastName: ""
+    }
+  },
+  methods:{
+    onSubmit(){
+      console.log("Registering developer");
+      this.$emit('developer-registered',{firstName:this.firstName,lastName: this.lastName});
+    }
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
