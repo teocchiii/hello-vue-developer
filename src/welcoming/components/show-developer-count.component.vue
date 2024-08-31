5d0ab7ff75f5effc74af4cbd787a0dee3528b5ef
Daniel Del Castillo <u202211212@upc.edu.pe>
2024-08-31 12:05:21 -0500

    feat(welcoming): added show developer count component.

---
+++ src/welcoming/components/show-developer-count.component.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: "show-developer-count.component",
+  props:{
+    developerCount:{ type:Number, required:true }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <p>Developers registerd: {{developerCount}}</p>
+    <p>Note:Unknown developers are not considered.</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
