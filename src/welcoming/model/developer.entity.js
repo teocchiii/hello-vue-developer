c680ac90a1edd5f896f465921cdc09dfa0a3aa18
Daniel Del Castillo <u202211212@upc.edu.pe>
2024-08-31 11:50:55 -0500

    feat(welcoming): added developer entity.

---
+++ src/welcoming/model/developer.entity.js
@@ -0,0 +1,16 @@
+export class Developer{
+    firstName;
+    lastName;
+
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    get fullName(){
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+
+
+}
